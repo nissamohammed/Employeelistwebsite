517de6e7c6c82cd2d01ddb3d84da2bb6ef9d08e4
nissamohammed <nissamohammed8@gmail.com>
2024-12-07 19:39:59 +0530

    serverurl changed

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,2 +1,2 @@
-//export const serverUrl = 'https://employeelist-serverside.onrender.com'
-export const serverUrl = 'http://localhost:4000'
+export const serverUrl = 'https://employeelist-serverside.onrender.com'
+//export const serverUrl = 'http://localhost:4000'
